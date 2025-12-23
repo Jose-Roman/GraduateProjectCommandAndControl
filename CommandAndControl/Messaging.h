6f5bf9b26095944d0af7f0aba009ee96398a0acc
Jose Roman <joseroman@Joses-MacBook-Air.local>
2025-12-22 17:23:11 -0800

    Message Interface Header

--- CommandAndControl/Messaging.h
+++ CommandAndControl/Messaging.h
@@ -2,12 +2,37 @@
 // Created by Jose Roman  on 12/22/25.
 //
 
-#ifndef
-#define 
+#ifndef MESSAGING_H
+#define MESSAGING_H
 
+#include <string>
+#include <functional>
+#include <unordered_map>
+#include <vector>
 
-class Messaging {
+struct Message {
+    std::string topic;
+    std::string payload;
+    int timestamp;
+    int messageId;
+    std::string source;
 };
 
+class Messaging {
+    public:
+        using Callback = std::function<void(Message &)>;
+        Messaging() = default;
+        ~Messaging() = default;
 
-#endif //
+        //Publish a message to all subscribers of the topic
+        void publish(const Message& msg);
+
+        //Subscribe to a callback of a specific topic
+        void subscribe(const std::string& topic, Callback cb);
+
+    private:
+        std::unordered_map<std::string, std::vector<Callback>> subscribers_;
+        int messageCounter_ = 0;
+};
+
+#endif // MESSAGING_H
