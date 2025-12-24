8b3de5cf6c483391839e3d41fc43d905c7c6c044
Jose Roman <joseroman@Joses-MacBook-Air.local>
2025-12-24 15:20:37 -0800

    command executor header

--- CommandAndControl/CommandExecutor.h
+++ CommandAndControl/CommandExecutor.h
@@ -4,9 +4,22 @@
 
 #ifndef COMMANDEXECUTOR_H
 #define COMMANDEXECUTOR_H
+#include <string>
+#include "Messaging.h"
 
 
 class CommandExecutor {
+    public:
+        explicit  CommandExecutor(Messaging& messaging);
+
+        void initialize();
+        void executeCommand(const std::string& command);
+
+    private:
+        Messaging& messaging_;
+
+        void onCommandMessage(const Message& msg);
+
 };
 
 
