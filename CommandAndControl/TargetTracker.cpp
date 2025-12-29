1b1fa3c3be0e1002b0288d8fde26286561b7e0ed
Jose Roman <joseroman@Joses-MacBook-Air.local>
2025-12-29 14:58:45 -0800

    Start of target tracker implementation

--- CommandAndControl/TargetTracker.cpp
+++ CommandAndControl/TargetTracker.cpp
@@ -10,12 +10,44 @@
 TargetTracker::TargetTracker(Messaging &messaging) : messaging_(messaging) {}
 
 // Subscribes to sensor data messages so targets can be tracked
-void TargetTracker::initialize() {}
+void TargetTracker::initialize() {
+    messaging_.subscribe("sensor.data",
+        [this](const Message& msg) {
+            onSensorDataReceived(msg);
+        });
+
+    std::cout << "[TargetTracker] Initialized and listening for sensor data"
+                << std::endl;
+}
 
 // Callback invoked when sensor data is received
 void TargetTracker::onSensorDataReceived(const Message& msg) {
+    std::cout << "[TargetTracker] Sensor Data Received: "
+                << msg.payload << std::endl;
 
+    Target track = trackTarget();
+    Message targetMsg;
+    targetMsg.topic = "Target Tracked";
+
+    std::ostringstream payload;
+    payload << "Target ID = " << track.id
+            << "Position = ("<< track.x  << ", " << track.y << ")";
+    targetMsg.payload = payload.str();
+
+    messaging_.publish(targetMsg);
 }
 
 // Simulates basic target tracking logic
-Target TargetTracker::trackTarget() {}
+// TODO : make a helper randomizer script that will generate simulated
+// data that gives us long, lat, etc position values to simulate
+// real world command and control environment
+Target TargetTracker::trackTarget() {
+    static int targetCounter = 0;
+
+    Target target;
+    target.id = ++targetCounter;
+    target.x = 100.0;
+    target.y = 200.0;
+
+    std::cout << "[TargetTracker] Tracking Target:" << target.id << std::endl;
+}
