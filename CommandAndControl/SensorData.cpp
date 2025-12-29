584f9b33dd9ed4859916a35b06108f63cdf4dfa1
Jose Roman <joseroman@Joses-MacBook-Air.local>
2025-12-28 17:18:58 -0800

    Sensor Data cpp

--- CommandAndControl/SensorData.cpp
+++ CommandAndControl/SensorData.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "SensorData.h"
+#include <iostream>
+
+// Constructor
+SensorData::SensorData(Messaging &messaging) : messaging_(messaging){}
+
+// Initialize the sensor system
+void SensorData::initialize() {
+    std::cout << "[SensorData] Sensor initialized" << std::endl;
+}
+
+// Simulates retriving data from the system
+SensorReading SensorData::readSensor() const {
+    SensorReading reading;
+    // TODO sensor name and value
+    // default values for now
+    reading.sensorName = "TEMP NAME";
+    reading.value = 60.0;
+    return reading;
+}
+
+// Publishes sensor data to the messaging system
+void SensorData::publishSensorData() {
+    SensorReading reading = readSensor();
+    std::cout << "[SensorData] Sensor Reading: "
+                << reading.sensorName << " = "
+                << reading.value << std::endl;
+
+    Message msg;
+    msg.topic = "SensorUpdate";
+    msg.payload = reading.sensorName + " = " + std::to_string(reading.value);
+    msg.source = "SensorData";
+
+    // Publish message
+    messaging_.publish(msg);
+}
