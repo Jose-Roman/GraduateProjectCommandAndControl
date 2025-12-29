ffbc3a9509095fbc01fad8ececaf6ead068e5228
Jose Roman <joseroman@Joses-MacBook-Air.local>
2025-12-28 16:57:17 -0800

    sensor data header

--- CommandAndControl/SensorData.h
+++ CommandAndControl/SensorData.h
@@ -4,10 +4,32 @@
 
 #ifndef SENSORDATA_H
 #define SENSORDATA_H
+#include "Messaging.h"
+#include <string>
 
-
-class SensorData {
+// Struct representing a sensor reading
+struct SensorReading {
+    std::string sensorName;
+    double value;
 };
 
+class SensorData {
+    public:
+        // Constructor that makes reference to messaging system
+        explicit SensorData(Messaging& messaging);
+
+        // Initializes the sensor system abd prepares it to publish data
+        void initialize();
+
+        // Simulates reading data from a sensor
+        // TODO: generate data that represents sensor activites
+        SensorReading readSensor() const;
+
+        // Publishes a sensor update message
+        void publishSensorData();
+
+    private:
+        Messaging& messaging_;
+};
 
 #endif //SENSORDATA_H
