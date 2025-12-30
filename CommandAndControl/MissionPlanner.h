4e39d665f0fa3de5a62962f969bc697ad6ad13c5
Jose Roman <joseroman@Joses-MacBook-Air.local>
2025-12-30 11:11:10 -0800

    mission planner header

--- CommandAndControl/MissionPlanner.h
+++ CommandAndControl/MissionPlanner.h
@@ -5,8 +5,25 @@
 #ifndef MISSIONPLANNER_H
 #define MISSIONPLANNER_H
 
+#include <string>
+#include "Messaging.h"
+#include "TargetTracker.h"
 
 class MissionPlanner {
+    public:
+        // Constructor that accepts a reference to the messaging system
+        explicit MissionPlanner(Messaging& messaging);
+
+        // Registers the MissionPlanner to receive targer tracking updates
+        void initialize();
+    private:
+        Messaging& messaging_;
+
+        // Callback invoked when a target tracking message is received
+        void onTargetReceived(const Message& msg) const;
+
+        // Creates and publishes a command based on target data
+        void planMission(const Target& track) const;
 };
 
 
