9166ef21772d306da2852d90ff855999264814ee
Jose Roman <joseroman@Joses-MacBook-Air.local>
2026-01-29 19:53:46 -0800

    Set up GTEST

---
+++ CommandAndControl/test/TargetTrackerTest.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Jose Roman  on 1/29/26.
+//
+
+#include <gtest/gtest.h>
+
+#include "TargetTracker.h"
+#include "Geography.h"
+#include "MissionPlanner.h"
+#include "Messaging.h"
+
+class TargetTrackerTest : public ::testing::Test {
+protected:
+
+};
+
+TEST_F(TargetTrackerTest, TrackTargetCreatesValidTarget) {
+
+}
+
+TEST_F(TargetTrackerTest, UpdateTrackIncrementsUpdateCounter) {
+
+}
+
+TEST_F(TargetTrackerTest, UpdateTrackChangesPosition) {
+
+}
+
+
