114c82cb439f88907454876b47a028a84a509d4e
Jose Roman <joseroman@Joses-MacBook-Air.local>
2026-02-01 14:10:14 -0800

    Target Tracker UT

--- CommandAndControl/test/TargetTrackerTest.cpp
+++ CommandAndControl/test/TargetTrackerTest.cpp
@@ -11,19 +11,53 @@
 
 class TargetTrackerTest : public ::testing::Test {
 protected:
+    Messaging msg;
+    Geography geo;
+
+    TargetTracker targetTracker;
+
+    TargetTrackerTest() : targetTracker(msg, geo) {}
 
 };
 
-TEST_F(TargetTrackerTest, TrackTargetCreatesValidTarget) {
+TEST_F(TargetTrackerTest, TrackTargetCreatesValidTarget)
+{
+    Target t = targetTracker.trackTarget(MissionArea::LosAngeles,
+                                         TargetType::Plane);
 
+    EXPECT_GT(t.id, 0);
+    EXPECT_EQ(t.type, TargetType::Plane);
 }
 
-TEST_F(TargetTrackerTest, UpdateTrackIncrementsUpdateCounter) {
+TEST_F(TargetTrackerTest, UpdateTrackIncrementsUpdateCounter)
+{
+    Target t = targetTracker.trackTarget(MissionArea::LosAngeles,
+                                         TargetType::Plane);
 
+    targetTracker.addActiveTrack(t);
+
+    targetTracker.updateTrack(t.id, 30);  // 30 seconds
+
+    Target updated = targetTracker.getActiveTrack(t.id);
+
+    EXPECT_GT(updated.numOfUpdates, 0);
 }
 
-TEST_F(TargetTrackerTest, UpdateTrackChangesPosition) {
+TEST_F(TargetTrackerTest, UpdateTrackChangesPosition)
+{
+    Target t = targetTracker.trackTarget(MissionArea::LosAngeles,
+                                         TargetType::Plane);
 
+    double latBefore = t.latitude;
+    double lonBefore = t.longitude;
+
+    targetTracker.addActiveTrack(t);
+
+    targetTracker.updateTrack(t.id, 30);
+
+    Target updated = targetTracker.getActiveTrack(t.id);
+
+    EXPECT_TRUE(updated.latitude != latBefore ||
+                updated.longitude != lonBefore);
 }
-
 
