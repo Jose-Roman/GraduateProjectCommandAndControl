0e7830c3a2e74ba299869d079d807a532321253f
Jose Roman <joseroman@Joses-MacBook-Air.local>
2026-02-03 19:13:57 -0800

    Geography UTs

---
+++ CommandAndControl/test/GeographyTest.cpp
@@ -0,0 +1,59 @@
+//
+// Created by Jose Roman  on 2/3/26.
+//
+
+#include <gtest/gtest.h>
+
+#include "Geography.h"
+#include "Types.h"
+
+class GeographyTest : public ::testing::Test {
+protected:
+    Geography geo;
+};
+
+TEST_F(GeographyTest, GetGeoBoundsReturnsCorrectLosAngelesBounds) {
+    MissionArea area = MissionArea::LosAngeles;
+
+    GeoBounds bounds = geo.getGioBounds(area);
+
+    EXPECT_DOUBLE_EQ(bounds.minLatitude, 33.7);
+    EXPECT_DOUBLE_EQ(bounds.maxLatitude, 34.4);
+    EXPECT_DOUBLE_EQ(bounds.minLongitude, -118.9);
+    EXPECT_DOUBLE_EQ(bounds.maxLongitude, -117.6);
+}
+
+TEST_F(GeographyTest, GetGeoKinematicsReturnsPlaneValues) {
+    TargetType type = TargetType::Plane;
+
+    GeoKinematics kin = geo.getGioKinematics(type);
+
+    EXPECT_EQ(kin.minAltitude, 5000);
+    EXPECT_EQ(kin.maxAltitude, 40000);
+    EXPECT_EQ(kin.minSpeed, 200);
+    EXPECT_EQ(kin.maxSpeed, 600);
+}
+
+TEST_F(GeographyTest, RandomDoubleWithinBounds) {
+    double min = 10.0;
+    double max = 20.0;
+
+    for (int i = 0; i < 100; ++i) {
+        double value = geo.randomDouble(min, max);
+
+        EXPECT_GE(value, min);
+        EXPECT_LE(value, max);
+    }
+}
+
+TEST_F(GeographyTest, GetSensorLocationMiami) {
+    MissionArea area = MissionArea::Miami;
+
+    sensorLocation loc = geo.getSensorLocation(area);
+
+    EXPECT_DOUBLE_EQ(loc.latitude, 25.7617);
+    EXPECT_DOUBLE_EQ(loc.longitude, -80.1918);
+}
+
+
+
