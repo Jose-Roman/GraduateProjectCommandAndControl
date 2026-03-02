022817f64240f79c13815a4ac9e79531bf6142d3
Jose Roman <joseroman@Joses-MacBook-Air.local>
2026-03-01 20:47:09 -0800

    types testing py

--- pythonScripts/test_types.py
+++ pythonScripts/test_types.py
@@ -9,3 +9,10 @@ print("types_py module loaded")
 g = geo.Geography()
 
 bounds = g.getGeoBounds(types.MissionArea.LosAngeles)
+
+print("----Los Angeles Mission Geo Bounds----")
+print("Area Bounds")
+print("Min Latitude: ", bounds.minLatitude)
+print("Max Latitude: ", bounds.maxLatitude)
+print("Min Longitude: ", bounds.minLongitude)
+print("Max Longitude: ", bounds.maxLongitude)
